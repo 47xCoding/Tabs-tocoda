734c6f22e6488f094293983127d9935dddb9cb5a
47xCoding <74483645+47xCoding@users.noreply.github.com>
2021-06-29 18:04:53 +0600

    Scripted everything.
    Added all the necessary scripts to click to hide other articles and show the desired article.

---
+++ setup/app.js
@@ -0,0 +1,21 @@
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+// add event
+about.addEventListener('click', function (e) {
+ const id = e.target.dataset.id;
+ if (id) {
+  btns.forEach(function (btn) {
+   // remove active class from other buttons
+   btn.classList.remove("active");
+   e.target.classList.add("active");
+  })
+  // remove active class from other articles
+  articles.forEach(function (article) {
+   article.classList.remove("active");
+   const element = document.getElementById(id);
+   element.classList.add("active");
+  })
+ }
+})
